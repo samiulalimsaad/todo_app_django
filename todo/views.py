292af6288b159c981066ff249c73d7dce84e914c
samiulalimsaad <samiulalimsaad@gmail.com>
2023-08-28 00:52:09 +0600

    todo edit controller now functional

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
@@ -18,7 +19,6 @@ def index(req) -> HttpResponse:
     if req.user.is_authenticated:
         user = req.user
         if req.method == "POST":
-            print(user)
             todo = Todo(
                 user=user,
                 title=req.POST["title"],
@@ -26,26 +26,49 @@ def index(req) -> HttpResponse:
                 due_date=req.POST["due_date"],
                 status=StatusEnum.Pending,
             )
-            print(todo)
             todo.save()
             todos = Todo.objects.filter(user=user)
             return render(req, "todo.html", {"is_authenticated": True, "todos": todos})
         else:
             todos = Todo.objects.filter(user=user)
-            print(todos)
             return render(req, "todo.html", {"is_authenticated": True, "todos": todos})
     else:
         return render(req, "login.html", {"is_authenticated": False})
 
 
+@login_required(login_url="login/")
 def todo_detail(request, todo_id):
     todo = get_object_or_404(Todo, id=todo_id)
     return render(request, "todo_detail.html", {"todo": todo})
 
 
+@login_required(login_url="login/")
+def todo_delete(req, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id)
+    todo.delete()
+    return redirect("index")
+
+
+@login_required(login_url="login/")
+def todo_edit(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id)
+    if request.method == "GET":
+        return render(request, "todo_edit.html", {"todo": todo})
+    if request.method == "POST":
+        try:
+            instance = Todo.objects.get(pk=todo_id)
+        except Todo.DoesNotExist:
+            return render(request, "todo.html", {"todo": todo})
+
+        for key in request.POST:
+            setattr(instance, key, request.POST[key])
+        instance.save()
+        return redirect("index")
+    return render(request, "todo_detail.html", {"todo": todo})
+
+
 def register(request) -> HttpResponse:
     if request.method == "POST":
-        print("request.POST")
         name = request.POST["name"]
         email = request.POST["email"]
         password = request.POST["password"]
@@ -59,11 +82,9 @@ def register(request) -> HttpResponse:
 
 def login(request) -> HttpResponse:
     if request.method == "POST":
-        print(request.POST["name"], request.POST["password"])
         user = authenticate(
             request, username=request.POST["name"], password=request.POST["password"]
         )
-        print(user)
         if user is not None:
             login_user(request, user)
             return redirect("index")
