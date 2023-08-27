b499eb15f09d235fb4083c6edda0cdc5949d866e
samiulalimsaad <samiulalimsaad@gmail.com>
2023-08-27 23:42:20 +0600

    todo detail now functional

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("todo_detail/<int:todo_id>/", views.todo_detail, name="todo_detail"),
     path("signup/", views.register, name="signup"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,13 @@
-import email
-from django.shortcuts import redirect, render
+from datetime import datetime
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, logout as logout_user, login as login_user
+from todo.enums.status_enum import StatusEnum
+
+from todo.models import Todo
 
 
 # Create your views here.
@@ -13,15 +16,31 @@ from django.contrib.auth import authenticate, logout as logout_user, login as lo
 @login_required(login_url="login/")
 def index(req) -> HttpResponse:
     if req.user.is_authenticated:
-        user = User.objects.get()
-        print(req.user)
-        # Do something for authenticated users.
-        ...
-        return render(req, "todo.html", {"is_authenticated": True})
+        user = req.user
+        if req.method == "POST":
+            print(user)
+            todo = Todo(
+                user=user,
+                title=req.POST["title"],
+                description=req.POST["description"],
+                due_date=req.POST["due_date"],
+                status=StatusEnum.Pending,
+            )
+            print(todo)
+            todo.save()
+            todos = Todo.objects.filter(user=user)
+            return render(req, "todo.html", {"is_authenticated": True, "todos": todos})
+        else:
+            todos = Todo.objects.filter(user=user)
+            print(todos)
+            return render(req, "todo.html", {"is_authenticated": True, "todos": todos})
     else:
-        # Do something for anonymous users.
-        ...
         return render(req, "login.html", {"is_authenticated": False})
+
+
+def todo_detail(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id)
+    return render(request, "todo_detail.html", {"todo": todo})
 
 
 def register(request) -> HttpResponse:
