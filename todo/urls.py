caf0dccd0d8359592cbaeb3c7da6f6a6565f9ea7
samiulalimsaad <samiulalimsaad@gmail.com>
2023-08-28 00:52:18 +0600

    updated

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("todo_detail/<int:todo_id>/", views.todo_detail, name="todo_detail"),
+    path("todo_edit/<int:todo_id>/", views.todo_edit, name="todo_edit"),
+    path("todo_delete/<int:todo_id>/", views.todo_delete, name="todo_delete"),
     path("signup/", views.register, name="signup"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
